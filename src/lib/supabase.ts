2c3acd33f3ce4acc8ecf348bd80f71351ee11ac3
Yamini-maniar <yaminimaniar@gmail.com>
2025-12-30 21:18:34 +0530

    chore: remove supabase secret

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 import 'react-native-url-polyfill/auto';
 import { createClient } from '@supabase/supabase-js';
+import Config from 'react-native-config';
 
-const SUPABASE_URL = 'REMOVED';
-const SUPABASE_ANON_KEY =
-  'REMOVED';
+const SUPABASE_URL = Config.SUPABASE_URL ?? '';
+const SUPABASE_ANON_KEY = Config.SUPABASE_ANON_KEY ?? '';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
